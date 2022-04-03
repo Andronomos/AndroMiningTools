3bbacf1a0a390862076bfa5e2287e57030bed5b2
Andronomos <andronomos@inbox.com>
2022-04-03 14:07:11 -0700

    Update AndroMiningTools.java

--- src/main/java/andronomos/androminingtools/AndroMiningTools.java
+++ src/main/java/andronomos/androminingtools/AndroMiningTools.java
@@ -8,11 +8,13 @@ import andronomos.androminingtools.registry.ModItems;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+@Mod(AndroMiningTools.MOD_ID)
 public class AndroMiningTools {
 	public static final String MOD_ID = "androminingtools";
 	public static final String TAB_NAME = "androminingtools_group";
